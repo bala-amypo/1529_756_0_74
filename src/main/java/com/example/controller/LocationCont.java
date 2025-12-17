acc2d808e1d563e183eaa6c45f31e0091ee309b7
bala-amypo <bala@amypo.in>
2025-12-17 08:16:00 +0000

    User commit

---
+++ src/main/java/com/example/controller/LocationCont.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+
+import java.lang.reflect.AccessFlag.Location;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.service.LocationSer;
+
+@RestController
+public class LocationCont {
+    @Autowired
+LocationSer locationSer;
+@PostMapping("/a")
+public Location add(@RequestBody Location le  ){
+    return locationSer.createlocation(le);
+
+}
+@GetMapping("/showlocation")
+public List<Location> show(){
+    return locationSer.getalllocation();
+}
+}
