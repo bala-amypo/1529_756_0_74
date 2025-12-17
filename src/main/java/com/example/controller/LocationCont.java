0587a83a8bb2ef9169681d19eff60edee861ceea
bala-amypo <bala@amypo.in>
2025-12-17 08:45:59 +0000

    User commit

--- src/main/java/com/example/controller/LocationCont.java
+++ src/main/java/com/example/controller/LocationCont.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 
-import java.lang.reflect.AccessFlag.Location;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,19 +7,22 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.entity.Location;
 import com.example.demo.service.LocationSer;
 
 @RestController
 public class LocationCont {
     @Autowired
-LocationSer locationSer;
-@PostMapping("/a")
-public Location add(@RequestBody Location le  ){
-    return locationSer.createlocation(le);
+    LocationSer locationSer;
 
-}
-@GetMapping("/showlocation")
-public List<Location> show(){
-    return locationSer.getalllocation();
-}
+    @PostMapping("/a")
+    public Location add(@RequestBody Location le) {
+        return locationSer.createlocation(le);
+
+    }
+
+    @GetMapping("/showlocation")
+    public List<Location> show() {
+        return locationSer.getalllocation();
+    }
 }
--- src/main/java/com/example/demo/repository/LocationR.java
+++ src/main/java/com/example/demo/repository/LocationR.java
@@ -1,7 +1,8 @@
 package com.example.demo.repository;
+
 import com.example.demo.entity.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LocationR extends JpaRepository<Location,Long>{
+public interface LocationR extends JpaRepository<Location, Long> {
 
 }
