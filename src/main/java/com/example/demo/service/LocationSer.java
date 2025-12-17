caed53c76484e731f0e825fcd5c16ac07e179284
bala-amypo <bala@amypo.in>
2025-12-17 06:46:51 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/LocationSer.java
@@ -0,0 +1,11 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.entity.Location;
+
+public interface LocationSer {
+Location createlocation(Location location);
+List<Location>getalllocation();
+java.lang.reflect.AccessFlag.Location createlocation(java.lang.reflect.AccessFlag.Location le);
+}
