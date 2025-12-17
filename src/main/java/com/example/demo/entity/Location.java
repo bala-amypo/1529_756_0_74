0a943301629dc231a53016063397b1bd38b6f129
bala-amypo <bala@amypo.in>
2025-12-17 09:46:00 +0000

    User commit

--- src/main/java/com/example/demo/entity/Location.java
+++ src/main/java/com/example/demo/entity/Location.java
@@ -9,7 +9,7 @@ import jakarta.persistence.Id;
 public class Location {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
     private String name;
     private Double latitude;
     private Double longitude;
