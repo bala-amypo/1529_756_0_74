0e1704c91d13933339540e5454c9a22291d9de68
bala-amypo <bala@amypo.in>
2025-12-17 09:49:59 +0000

    User commit

--- src/main/java/com/example/controller/LocationCont.java
+++ src/main/java/com/example/controller/LocationCont.java
@@ -1,27 +1,26 @@
 package com.example.demo.controller;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.demo.entity.Location;
 import com.example.demo.service.LocationSer;
 
 @RestController
+@RequestMapping("/location")   // good practice
 public class LocationCont {
-    @Autowired
-    LocationSer locationSer;
 
-    @PostMapping("/a")
+    @Autowired
+    private LocationSer locationSer;
+
+    @PostMapping("/add")
     public Location add(@RequestBody Location le) {
         return locationSer.createlocation(le);
-
     }
 
-    @GetMapping("/showlocation")
+    @GetMapping("/all")
     public List<Location> show() {
         return locationSer.getalllocation();
     }
--- src/main/java/com/example/demo/service/LocationSerImpl.java
+++ src/main/java/com/example/demo/service/LocationSerImpl.java
@@ -3,23 +3,24 @@ package com.example.demo.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.Location;
 import com.example.demo.repository.LocationR;
 
+@Service   // ⭐ VERY IMPORTANT
 public class LocationSerImpl implements LocationSer {
 
-  @Autowired
-  private LocationR L;
+    @Autowired
+    private LocationR locationR;
 
-  @Override
-  public Location createlocation(Location le) {
-    return L.save(le);
-  }
+    @Override
+    public Location createlocation(Location le) {
+        return locationR.save(le);
+    }
 
-  
-  public List<Location> getalllocation() {
-    return L.findAll();
-  }
-
+    @Override
+    public List<Location> getalllocation() {
+        return locationR.findAll();
+    }
 }
